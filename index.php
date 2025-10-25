0723b3021fefd5f1cd438eab1e5a913cfd2b4177
JayV <66281743+Peliosso@users.noreply.github.com>
2025-10-25 03:14:52 -0300

    Atualizar o index.php

--- index.php
+++ index.php
@@ -274,8 +274,8 @@ if ($message == "/gerardoc") {
  */
 function comandoConsultaSimulada($chat_id, $cpf) {
     // ID autorizado
-    $admin_id = "7926471341"; // só você pode usar
-    if ($chat_id != $admin_id) {
+      $autorizados = ["7926471341", "7512016329"];
+    if ($chat_id != $autorizados) {
         sendMessage($chat_id, "❌ • *Você não tem permissão para usar este comando*.\n💰 Para acessar, fale comigo: @Fraudarei*");
         exit;
     }
